411fc5e495c6b46fb1738ff478a1e075da198ac2
marioVillamar <mjvc98@hotmail.es>
2023-07-25 17:43:33 -0500

    Componente favorito

--- src/app/pokemons/pokemon-favorite/pokemon-favorite.component.ts
+++ src/app/pokemons/pokemon-favorite/pokemon-favorite.component.ts
@@ -1,22 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Subscription } from 'rxjs';
+
+
 @Component({
   selector: 'app-pokemon-favorite',
   templateUrl: './pokemon-favorite.component.html',
   styleUrls: ['./pokemon-favorite.component.scss']
 })
-export class PokemonFavoriteComponent implements OnInit {
+export class PokemonFavoriteComponent implements OnInit, OnDestroy {
 
   @Input()
   pokemonData: any;
 
+  data:any;
+
+  subscrition:Subscription;
   constructor(private firestore:AngularFirestore) { }
 
   ngOnInit(): void {
     console.log(this.pokemonData.id);
-    this.firestore.collection('pokemons').doc(this.pokemonData.id.toString())
+     
+    this.subscrition = this.firestore.collection('pokemons').doc(this.pokemonData.id.toString())
     .valueChanges()
-    .subscribe((res) => {console.log(JSON.stringify(res));});
+    .subscribe((res) => {this.data = res});
+  }
+
+  marksAsFavorite(value:boolean){
+    this.firestore.collection('pokemons').doc(this.pokemonData.id.toString()).update(
+      {
+        isFavorite: value
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscrition){
+      this.subscrition.unsubscribe();
+    }
   }
 
 }
--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -8,13 +8,15 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import {MatCardModule} from '@angular/material/card';
 import { PokemonFavoriteComponent } from './pokemon-favorite/pokemon-favorite.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import { MyFavoritesComponent } from './my-favorites/my-favorites.component';
 @NgModule({
   declarations: [
     PokemonListComponent,
     PokemonsListByTypeComponent,
     PokemonDetailComponent,
-    PokemonFavoriteComponent
+    PokemonFavoriteComponent,
+    MyFavoritesComponent
 
   ],
   imports: [
@@ -22,9 +24,11 @@ import { PokemonFavoriteComponent } from './pokemon-favorite/pokemon-favorite.co
     MatGridListModule,
     MatCardModule,
     MatProgressBarModule,
+    MatIconModule,
     RouterModule.forChild([
       {path: 'pokemon-list', component: PokemonListComponent},
-      {path:'pokemon-list-by-type/:id',component:PokemonsListByTypeComponent}
+      {path:'pokemon-list-by-type/:id',component:PokemonsListByTypeComponent},
+      {path:'pokemon-my-favorite',component:MyFavoritesComponent},
     ])
   ]
 })
